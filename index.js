df80cdb262a6e7c2861c1c893c433f2b661e8287
Trace <tracedelange@me.com>
2021-06-08 12:13:00 -0500

    commit tree 8c1244ffe52c161d958d59266f41a46b8d8592bc

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 
+
+function superbowlWin(record) {
+    const found = record.find(function(game, index){
+        if (game.result === "W") {
+            return true
+        } else {
+            return false
+        }
+    });
+    if (found !== undefined) {
+        return found.year
+    } else {
+        return undefined
+    }
+}
